0b5af8137652024da1e02ca3e554485680311e0e
DevDominik <71924117+DevDominik@users.noreply.github.com>
2024-09-05 10:35:27 +0200

    Add files via upload

--- órai feladat/script.js
+++ órai feladat/script.js
@@ -2,8 +2,6 @@
 
 let ev = new Date().getFullYear();
 
-// "hónap neve": {nap száma: "szöveg"}
-
 let honapokNevei = [
     "Január",
     "Február",
@@ -19,20 +17,37 @@ let honapokNevei = [
     "December"
 ];
 
+
+// "hónap neve": max napok száma
+let honapokMaxNap = {};
+
+// "hónap neve": {nap száma: "szöveg"}
 let honapokTaroltAdat = {
     
 };
 
 let honapCsere = (honapNeve) => {
-    
+    document.getElementById("divNapok").innerHTML = "";
+    for (let napIndex = 1; napIndex < honapokMaxNap[honapNeve] + 1; napIndex++) {
+        let ujDiv = document.createElement("div");
+        ujDiv.id = `divHonap-${honapNeve}-${napIndex}`;
+        let ujBekezdes = document.createElement("p");
+        ujBekezdes.innerHTML = napIndex + ".";
+        let ujSzovegesMezo = document.createElement("input");
+        ujSzovegesMezo.type = "text";
+        ujSzovegesMezo.value = ""
+        ujSzovegesMezo.id = `txt-${honapNeve}-${napIndex}`
+    };
 };
 
 for (let honapSzama = 1; honapSzama < 13; honapSzama++) {
-    let jelenlegiHonap = new Date(ev, honapSzama);
+    let jelenlegiHonap = new Date(ev, honapSzama, 0);
     let napokAdatok = {};
-    for (let napSzama = 1; napSzama < jelenlegiHonap.get; index++) {
-        const element = array[index];
-        
-    }
+    for (let napSzama = 1; napSzama < jelenlegiHonap.getDate() + 1; index++) {
+         napokAdatok[napSzama] = ""
+    };
+    honapokMaxNap[honapokNevei[honapSzama - 1]] = jelenlegiHonap.getDate()
     honapokTaroltAdat[honapokNevei[honapSzama - 1]] = napokAdatok;
 };
+
+honapCsere("Január")
